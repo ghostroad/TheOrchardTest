e9ffd17a478be8d84affc4a14ee063687e339fbb
Mushfeq <mushfeq.khan@gmail.com>
2019-04-09 15:24:31 -0400

    Adding a PUT endpoint for creating/updating establishments.

--- sietsema/blueprints.py
+++ sietsema/blueprints.py
@@ -11,6 +11,21 @@ def health():
 @write_api.route('/establishment/<int:camis>', methods=['PUT'])
 def establishment(camis):
     data = request.get_json()
-    db.session.add(Establishment(camis=camis, **data))
-    db.session.commit()
-    return jsonify(success=True)
+
+    dba = data['dba']
+    if not dba:
+        return (jsonify(message="The 'dba' field cannot be empty."), 400)
+
+    existing = Establishment.query.get(camis)
+    if existing:
+        for attr in ['dba', 'boro', 'building', 'street', 'zipcode', 'phone', 'cuisine']:
+            if attr in data:
+                setattr(existing, attr, data[attr])
+        db.session.commit()
+        return jsonify(message="Updated existing establishment.")
+    else:
+        db.session.add(Establishment(camis=camis, **data))
+        db.session.commit()
+        return jsonify(message="Created new establishment.")
+    
+    
--- test/test_api.py
+++ test/test_api.py
@@ -1,13 +1,33 @@
+import json
 
 def test_health(test_client):
     response = test_client.get('/health')
     assert response.status_code == 200
     
-
 def test_creating_an_establishment(test_client, repo):
     data = {'dba': 'La Banquisse', 'phone': '4384056262'}
     response = test_client.put('/establishment/1234', json=data, content_type='application/json')
     assert response.status_code == 200
     
-    assert(repo.find(1234).dba == 'La Banquisse')
+    assert "Created" in json.loads(response.data)['message']
     
+    assert repo.find(1234).dba == 'La Banquisse'
+
+def test_updating_an_establishment(test_client, repo):
+    response = test_client.put('/establishment/1234', json={'dba': 'La Banquisse', 'phone': '4384056262'}, content_type='application/json')
+    assert response.status_code == 200
+
+    response = test_client.put('/establishment/1234', json={'dba': 'Romado', 'phone': '4384056363'}, content_type='application/json')
+    assert response.status_code == 200
+    
+    assert "Updated" in json.loads(response.data)['message']
+    
+    romado = repo.find(1234)
+    assert romado.dba == 'Romado'
+    assert romado.phone == '4384056363'
+
+def test_establishments_must_have_a_nonempty_dba(test_client, repo):
+    response = test_client.put('/establishment/1234', json={'dba': '', 'phone': '4384056262'}, content_type='application/json')
+    assert response.status_code == 400
+
+    assert "The 'dba' field cannot be empty." in json.loads(response.data)['message']        
