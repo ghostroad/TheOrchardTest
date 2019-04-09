ad4f5ce209e8c432a32e124887ff178008719e98
Mushfeq <mushfeq.khan@gmail.com>
2019-04-09 12:19:06 -0400

    Reorganizing the tests.

--- test/test_establishment.py
+++ test/test_establishment.py
@@ -1,19 +1,26 @@
 import pytest
-from app.models import Establishment
+from app.models import Establishment, Rating
 from sqlalchemy import exc
 
-def test_creation(test_db):
-    test_db.add(Establishment(camis = 1234, dba = "Brasserie Beaubien"))
-    test_db.commit()
+def test_creation(repo):
+    repo.save(Establishment(camis = 1234, dba = "Brasserie Beaubien"))
     
     assert(len(Establishment.query.all()) == 1)
     
-def test_uniqueness_of_camis_is_enforced(test_db):
-    test_db.add(Establishment(camis = 1234, dba = "Brasserie Beaubien"))
-    test_db.add(Establishment(camis = 1234, dba = "Next Door"))
-    
+def test_uniqueness_of_camis_is_enforced(repo):
     with pytest.raises(exc.IntegrityError):
-        test_db.commit()
-    
-    
-    
+        repo.save(
+            Establishment(camis = 1234, dba = "Brasserie Beaubien"), 
+            Establishment(camis = 1234, dba = "Next Door")
+        )
+        
+def test_latest_rating(repo):        
+    repo.save(Establishment(camis=3456, dba="Pho Lien"))
+    repo.save(Establishment(camis=1234, dba="Brasserie Beaubien", ratings=[
+        Rating(grade="A", date="01/02/19"), 
+        Rating(grade="B", date="03/02/19"), 
+        Rating(grade="C", date="02/02/19")
+    ]))
+
+    assert(Establishment.query.get(1234).latest_rating.grade=="B")
+    assert(Establishment.query.get(3456).latest_rating is None)
--- test/test_rating.py
+++ test/test_rating.py
@@ -1,6 +1,5 @@
 import pytest
-from app.models import Establishment, Rating, LatestRating
-from sqlalchemy import exc, or_
+from app.models import Establishment, Rating
 from datetime import date
 from app.repositories import EstablishmentRepository
 
@@ -13,40 +12,3 @@ def test_creation(repo):
     assert(rating.date == date(2019, 1, 2))
     assert(rating.grade == "A")
             
-    
-def test_latest_rating(repo):        
-    repo.save(Establishment(camis=3456, dba="Pho Lien"))
-    repo.save(Establishment(camis=1234, dba="Brasserie Beaubien", ratings=[
-        Rating(grade="A", date="01/02/19"), 
-        Rating(grade="B", date="03/02/19"), 
-        Rating(grade="C", date="02/02/19")
-    ]))
-
-    assert(Establishment.query.get(1234).latest_rating.grade=="B")
-    assert(Establishment.query.get(3456).latest_rating is None)
-    
-def test_filtering_by_latest_rating(repo):
-    poor = Establishment(camis=3456, dba="Pho Lien", ratings=[
-        Rating(grade="B", date="01/15/18"),
-        Rating(grade="C", date="06/15/18"),
-    ])
-
-    okay = Establishment(camis=1234, dba="Brasserie Beaubien", cuisine="French", ratings=[
-        Rating(grade="A", date="01/02/19"),
-        Rating(grade="B", date="03/02/19"),
-        Rating(grade="C", date="02/02/19")
-    ])
-    
-    excellent = Establishment(camis=7654, dba="Trou de Beigne", cuisine="French", ratings=[
-        Rating(grade="A", date="04/02/19"),
-        Rating(grade="C", date="02/02/19")
-    ])
-
-    repo.save(poor, okay, excellent)
-
-    okay_establishments = repo.latest_ratings_query().filter(
-        or_(Establishment.cuisine == "French", LatestRating.grade=="B", LatestRating.grade=="A")
-    ).all()
-
-    assert(set(okay_establishments) == {okay, excellent})
-    assert(set(establishment.latest_rating.grade for establishment in okay_establishments) == {"A", "B"})
